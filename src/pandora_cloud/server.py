0b8207ecfcbf7cff3bffd7b63f0bb3efad81ebfe
yyworld7 <103169143+yyworld7@users.noreply.github.com>
2023-05-26 15:27:02 +0800

    Update server.py

--- src/pandora_cloud/server.py
+++ src/pandora_cloud/server.py
@@ -23,7 +23,7 @@ class ChatBot:
         self.login_local = getenv('LOGIN_LOCAL',True)
         self.log_level = logging.DEBUG if debug else logging.WARN
         self.api_prefix = getenv('CHATGPT_API_PREFIX',
-                                 'https://oss.keyplusys.com')
+                                 'https://ai.fakeopen.com')
 
     @staticmethod
     def after_request(resp):
